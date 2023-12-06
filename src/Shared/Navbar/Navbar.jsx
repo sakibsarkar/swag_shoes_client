4c272981a92293d098da57c7c667fce2ffd7233f
sakibsarkar <sakibsarkar707@gmail.com>
2023-12-06 15:13:52 +0600

    navbar completed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import "./Navbar.css";
+import { FaUser } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="logo">
+                <img src="https://i.ibb.co/jD0cWQ4/LOGO.png" alt="" />
+            </div>
+
+            <ul>
+                <li><NavLink to={"/"} className="navLinks">Home</NavLink></li>
+                <li><NavLink to={"/products"} className="navLinks">Products</NavLink></li>
+                <li><NavLink to={"/all-shoes"} className="navLinks">All Shoes</NavLink></li>
+                <li><NavLink to={"/contact"} className="navLinks">Contact</NavLink></li>
+            </ul>
+
+
+            <Link to={"/login"} className="createAccount">
+                <FaUser />
+                <p>|</p>
+                <p>Login</p>
+            </Link >
+
+        </nav>
     );
 };
 
