55adbd345b47866245314e989a1ba4127eec9e01
sakibsarkar <sakibsarkar707@gmail.com>
2023-12-15 17:03:54 +0600

    statistic page added

--- src/AdminPage/Dashbar/Dashbar.jsx
+++ src/AdminPage/Dashbar/Dashbar.jsx
@@ -1,4 +1,5 @@
 import "./Dashbar.css";
+import { useState } from "react";
 import { FaBox } from "react-icons/fa";
 import { GrLineChart } from "react-icons/gr";
 import { MdOutlinePendingActions } from "react-icons/md";
@@ -6,26 +7,33 @@ import { MdOutlineFreeCancellation } from "react-icons/md";
 import { Link, NavLink } from "react-router-dom";
 
 const Dashbar = () => {
+
+
+
+
     return (
-        <div>
+        <>
 
 
-            <Link to={"/"} className="dashlogo">
-                <img src="https://i.ibb.co/jD0cWQ4/LOGO.png" alt="" />
-            </Link>
+            <div className="dashBarCon">
+                <Link to={"/"} className="dashlogo">
+                    <img src="https://i.ibb.co/jD0cWQ4/LOGO.png" alt="" />
+                </Link>
 
 
-            <h1>Dashboard</h1>
+                <h1>Dashboard</h1>
 
-            <div className="dashBoardLinks">
-                <NavLink to={"/dashboard/statistics"}><GrLineChart />Statistics</NavLink>
-                <NavLink to={"/dashboard/allOrders"}><FaBox /> All Orders</NavLink>
-                <NavLink to={"/dashboard/pendingOrders"}><MdOutlinePendingActions />Pending Orders</NavLink>
-                <NavLink to={"/dashboard/cancelRequest"}><MdOutlineFreeCancellation />Cancel Request</NavLink>
+                <div className="dashBoardLinks">
+                    <NavLink to={"/dashboard/statistics"}><GrLineChart />Statistics</NavLink>
+                    <NavLink to={"/dashboard/allOrders"}><FaBox /> All Orders</NavLink>
+                    <NavLink to={"/dashboard/pendingOrders"}><MdOutlinePendingActions />Pending Orders</NavLink>
+                    <NavLink to={"/dashboard/cancelRequest"}><MdOutlineFreeCancellation />Cancel Request</NavLink>
+                </div>
             </div>
 
+        
 
-        </div>
+        </>
     );
 };
 
--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,21 +1,66 @@
 import "./DashLayout.css";
 import Dashbar from "../AdminPage/Dashbar/Dashbar";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { FaBox } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrLineChart } from "react-icons/gr";
+import { MdOutlineFreeCancellation, MdOutlinePendingActions } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
+
+    const [showDrawer, setShowDrawer] = useState(false)
+
     return (
         <div className="dashboardContainer">
-            <div className="dashboarBar">
-                <Dashbar />
+
+            <div className="dashboarNavbar">
+                {
+                    showDrawer ?
+                        <RxCross2 onClick={() => setShowDrawer(!showDrawer)} />
+                        :
+                        <GiHamburgerMenu onClick={() => setShowDrawer(!showDrawer)} />
+                }
             </div>
+
+            <div className="dashboarBar">
+                <Dashbar showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
+
+
+            </div>
+
+
+            {
+                showDrawer ?
+
+                    <div className="dashBarCon dashBoardDrawer" onClick={() => setShowDrawer(false)}>
+                        <Link to={"/"} className="dashlogo">
+                            <img src="https://i.ibb.co/jD0cWQ4/LOGO.png" alt="" />
+                        </Link>
+
+
+                        <h1>Dashboard</h1>
+
+                        <div className="dashBoardLinks">
+                            <NavLink to={"/dashboard/statistics"}><GrLineChart />Statistics</NavLink>
+                            <NavLink to={"/dashboard/allOrders"}><FaBox /> All Orders</NavLink>
+                            <NavLink to={"/dashboard/pendingOrders"}><MdOutlinePendingActions />Pending Orders</NavLink>
+                            <NavLink to={"/dashboard/cancelRequest"}><MdOutlineFreeCancellation />Cancel Request</NavLink>
+                        </div>
+                    </div>
+
+                    :
+
+                    ""
+            }
 
 
             <div className="dashOutlet">
 
                 <Outlet />
             </div>
-        </div>
+        </div >
     );
 };
 
