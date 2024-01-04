7ec4829fcadedc81da57aea0794f28175d0eaba4
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-04 15:16:49 +0600

    live servel api

--- src/Hooks & Functions/Axios/UseAxios.jsx
+++ src/Hooks & Functions/Axios/UseAxios.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Context } from "../AauthContext";
 
 export const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://swag-shoes-server.vercel.app/api",
     withCredentials: true
 })
 
