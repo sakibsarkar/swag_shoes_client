dbccc31028246365179b07914eff180e47ed07ac
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-07 20:52:08 +0600

    pagination added in product page in admin dashboard

--- src/AdminPage/Products/Products.jsx
+++ src/AdminPage/Products/Products.jsx
@@ -1,29 +1,44 @@
 import "./Products.css";
 import ProductCard from "../../AdminComponents/ProductCard/ProductCard";
 import UseAxios from "../../Hooks & Functions/Axios/UseAxios";
+import { Pagination } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { getItemFromLS } from "../../Hooks & Functions/locaStorage";
 
 const Products = () => {
     const token = getItemFromLS("token")
     const axios = UseAxios()
 
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalData, setTotalData] = useState(18)
+
     const { data = {} } = useQuery({
-        queryKey: ["myProducts"],
+        queryKey: ["myProducts", currentPage],
         queryFn: async () => {
-            const { data: productData } = await axios.get(`/all/shoes?token=${token}&&currentPage=${0}&&limit=${18}`)
+            const { data: productData } = await axios.get(`/all/shoes?token=${token}&&currentPage=${currentPage}&&limit=${18}`)
+            setTotalData(productData?.totalData ? productData.totalData : 0)
             return productData
         }
     })
 
+    const handleCurrentPage = (event, value) => {
+        setCurrentPage(value - 1)
+
+        // window.scroll(0, 0)
+    }
 
     return (
-        <div>
+        <div className="productWrapper">
             <h1>All products</h1>
             <div className="products_container">
                 {
                     data?.result?.map(product => <ProductCard key={product._id} product={product} />)
                 }
+            </div>
+
+            <div className="admin_pagination">
+                <Pagination count={Math.ceil(totalData / 18)} variant="outlined" shape="rounded" onChange={handleCurrentPage} />
             </div>
 
         </div>
