1806018c678418534d6ab01aeb500b016406f16c
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-08 12:05:53 +0600

    basic setup for product add form

--- src/AdminPage/Products/Products.jsx
+++ src/AdminPage/Products/Products.jsx
@@ -4,9 +4,18 @@ import UseAxios from "../../Hooks & Functions/Axios/UseAxios";
 import { Pagination } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { AiFillFileAdd } from "react-icons/ai";
+import { BsPlusLg } from "react-icons/bs";
 import { getItemFromLS } from "../../Hooks & Functions/locaStorage";
 
 const Products = () => {
+
+
+    const [shoeAddProductForm, setshowAddProductForm] = useState(false);
+    const [speedDial, setSpeedDial] = useState(false)
+
+
+
     const token = getItemFromLS("token")
     const axios = UseAxios()
 
@@ -21,7 +30,7 @@ const Products = () => {
             return productData
         }
     })
-    
+
 
     const handleCurrentPage = (event, value) => {
         setCurrentPage(value - 1)
@@ -40,6 +49,41 @@ const Products = () => {
             <div className="admin_pagination">
                 <Pagination count={Math.ceil(totalData / 18)} variant="outlined" shape="rounded" onChange={handleCurrentPage} />
             </div>
+
+
+            <div className="addProduct">
+
+                <div className="speedAction" style={speedDial ? { height: "50px" } : { height: "0px", top: "0px" }}>
+                    <p>Add Product</p>
+                    <button className="speedActionButton" onClick={() => setshowAddProductForm(!shoeAddProductForm)}><AiFillFileAdd /></button>
+                </div>
+
+                <button className="addProductBtn" style={speedDial ? { transform: "rotate(45deg)" } : {}} onClick={() => setSpeedDial(!speedDial)}>
+                    <BsPlusLg />
+                </button>
+            </div>
+
+            {
+                shoeAddProductForm ?
+
+                    <div className="add_product_form">
+                        <form>
+                            <div>
+                                <p>Product Name</p>
+                                <input type="text" placeholder="Poduct Name" name="name" required />
+                            </div>
+                            <div>
+                                <p>Product Price</p>
+                                <input type="text" placeholder="Poduct Name" name="price" required />
+                            </div>
+                            <div>
+                                <p>Product Image</p>
+                                <input type="file" accept="image/*" placeholder="Poduct Name" name="price" required />
+                            </div>
+                        </form>
+                    </div> : ""
+            }
+
 
         </div>
     );
